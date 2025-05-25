42ad4b0c5b5faea20f677f7ce5b3aabfd8889b76
RobertoTorres <roberxiri@gmail.com>
2025-05-25 13:09:40 +0200

    settings.py modificado

--- webplayground_project/webplayground/settings.py
+++ webplayground_project/webplayground/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-dev-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 # ALLOWED_HOSTS
-ALLOWED_HOSTS = ['webplayground-curso-backend-ibm-deploy.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'webplayground-curso-backend-ibm-deploy.onrender.com,localhost,127.0.0.1').split(',')
 
 # Application definition
 
