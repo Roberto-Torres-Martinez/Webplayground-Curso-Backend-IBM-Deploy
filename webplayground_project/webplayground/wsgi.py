efeddcec11b9de80be824070485846d83562cdaf
RobertoTorres <roberxiri@gmail.com>
2025-05-25 12:42:14 +0200

    archivo wsgi.py modificado

--- webplayground_project/webplayground/wsgi.py
+++ webplayground_project/webplayground/wsgi.py
@@ -8,6 +8,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Añadir la carpeta padre de esta (webplayground_project) al sys.path
+current_path = os.path.dirname(os.path.abspath(__file__))  # ruta a webplayground/
+project_path = os.path.dirname(current_path)                # ruta a webplayground_project/
+sys.path.append(project_path)
 
 from django.core.wsgi import get_wsgi_application
 
